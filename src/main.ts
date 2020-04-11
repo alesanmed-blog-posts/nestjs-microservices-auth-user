2410d3a0b565858286dc1ffdb3e01e872721ea71
Ale Sánchez <alesanchezmedina@gmail.com>
2020-04-11 14:30:03 +0200

    Removed unused configuration

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   app.connectMicroservice({
     transport: Transport.TCP,
